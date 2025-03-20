3a10f6c8abed0520443e0101267eacad2360e6df
Josue19-08 <josuemarin2009@hotmail.com>
2025-03-20 10:30:04 -0600

    feat: update homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
 // src/app/page.tsx
 import Image from "next/image";
-import MetaMaskIcon from "../components/MetaMaskIcon";
-import StorePage from './mockstore';
 
 export default function Home() {
 
@@ -9,7 +7,6 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       {/* MetaMask connection button */}
       <div className="absolute top-8 right-8">
-        <MetaMaskIcon />
       </div>
 
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
